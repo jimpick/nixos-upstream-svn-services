d63e8c6caa27e7d694733aa44116209b412c74b8
visser <visser@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2005-04-26 07:51:48 +0000

    abaris config
    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@2892 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ abaris.nix
@@ -0,0 +1,101 @@
+let {
+
+#  body = twikiService;
+  body = webServer;
+
+  pkgs = import pkgs/system/all-packages.nix {system = __currentSystem;};
+
+  instanceRootDir = "/home/wiki/web-server";
+
+  adminAddr = "wiki-master@cs.uu.nl";
+
+  canonicalName = "http://abaris.zoo.cs.uu.nl"; # !!! ugly
+  
+  webServer = import ./apache-httpd {
+    inherit (pkgs) stdenv substituter apacheHttpd coreutils;
+    
+    logDir = instanceRootDir + "/log";
+    stateDir = instanceRootDir + "/state";
+    hostName = "abaris.zoo.cs.uu.nl";
+    inherit adminAddr;
+
+    subServices = [
+      minWiki
+#      testWiki
+#      ptWiki
+#      stWiki
+#      stIntraWiki
+    ];
+  };
+
+  testWiki = (import ./twiki/twiki-instance.nix).twiki {
+
+    defaultUrlHost = canonicalName;
+
+    name          = "test-wiki";
+    
+    pubdir        = instanceRootDir + "/test-wiki/pub";
+    datadir       = instanceRootDir + "/test-wiki/data";
+
+    twikiName     = "Test Wiki";
+    scriptUrlPath = "/test/bin";
+    pubUrlPath    = "/test/pub";
+  };
+
+  minWiki = (import ./twiki/twiki-instance.nix).twiki {
+
+    defaultUrlHost = canonicalName;
+
+    name          = "min-wiki";
+    
+    pubdir        = instanceRootDir + "/min-wiki/pub";
+    datadir       = instanceRootDir + "/min-wiki/data";
+
+    twikiName     = "Minimal Wiki";
+    scriptUrlPath = "/min/bin";
+    pubUrlPath    = "/min/pub";
+  };
+
+  ptWiki = (import ./twiki/twiki-instance.nix).twiki {
+
+    defaultUrlHost = canonicalName;
+
+    name          = "pt-wiki";
+    
+    pubdir        = instanceRootDir + "/pt-wiki/pub";
+    datadir       = instanceRootDir + "/pt-wiki/data";
+
+    twikiName     = "Program Transformation Wiki";
+    scriptUrlPath = "/pt/bin";
+    pubUrlPath    = "/pt/pub";
+  };
+
+  stWiki = (import ./twiki/twiki-instance.nix).twiki {
+
+    defaultUrlHost = canonicalName;
+
+    name          = "st-wiki";
+    
+    pubdir        = instanceRootDir + "/st-wiki/pub";
+    datadir       = instanceRootDir + "/st-wiki/data";
+
+    twikiName     = "ST Wiki";
+    scriptUrlPath = "/st/bin";
+    pubUrlPath    = "/st/pub";
+  };
+
+  stIntraWiki = (import ./twiki/twiki-instance.nix).twiki {
+
+    defaultUrlHost = canonicalName;
+
+    name          = "st-intra-wiki";
+    
+    pubdir        = instanceRootDir + "/st-intra-wiki/pub";
+    datadir       = instanceRootDir + "/st-intra-wiki/data";
+
+    twikiName     = "ST IntraWiki";
+    scriptUrlPath = "/st-intra/bin";
+    pubUrlPath    = "/st-intra/pub";
+  };
+
+}
