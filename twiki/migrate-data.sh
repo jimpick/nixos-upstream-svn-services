e311109c698de6e783a23bc8102b7b8cc68e3969
visser <visser@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2005-04-26 13:19:52 +0000

    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@2903 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- twiki/migrate-data.sh
+++ twiki/migrate-data.sh
@@ -6,8 +6,9 @@ new=$2
 for dir in `find $old/data/* -type d -and -not -name TWiki`
 do
   echo "Copying files from $dir"
-  cp -r --reply=yes $dir $new/data/`basename $dir`
-  mv $new/data/`basename $dir`/WebContents.txt $new/data/`basename $dir`/WebLeftBar.txt 
+  mkdir -p $new/data/`basename $dir`
+  cp -r --reply=yes $dir/ $new/data/`basename $dir`
+  mv $new/data/`basename $dir`/WebContents.txt $new/data/`basename $dir`/WebLeftBar.txt
   mv $new/data/`basename $dir`/WebContents.txt,v $new/data/`basename $dir`/WebLeftBar.txt,v
 done
 
