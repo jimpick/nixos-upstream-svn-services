d9c3490993929f5ce3b76c081036146ef3105921
visser <visser@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2005-04-29 12:34:49 +0000

    separate server for pt wiki
    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@2939 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

---
+++ abaris-pt.nix
@@ -0,0 +1,51 @@
+{productionServer}:
+
+let {
+
+#  body = twikiService;
+  body = webServer;
+
+  pkgs = import pkgs/system/all-packages.nix {system = __currentSystem;};
+
+  instanceRootDir = "/home/wiki/web-server";
+
+  adminAddr = "wiki-master@program-transformation.org";
+
+  httpPort = if productionServer then "8090" else "8091";  # !!! ugly
+
+  hostName = "abaris.zoo.cs.uu.nl";
+  
+  canonicalName = "http://" + hostName + ":" + httpPort;
+
+  
+  webServer = import ./apache-httpd {
+    inherit (pkgs) stdenv substituter apacheHttpd coreutils;
+    
+    logDir = instanceRootDir + "/log";
+    stateDir = instanceRootDir + "/state";
+    inherit hostName httpPort adminAddr;
+
+    subServices = [
+      ptWiki
+    ];
+  };
+
+  ptWiki = (import ./twiki/twiki-instance.nix).twiki {
+
+    defaultUrlHost = canonicalName;
+
+    name          = "pt-wiki";
+    
+    pubdir        = instanceRootDir + "/pt-wiki/pub";
+    datadir       = instanceRootDir + "/pt-wiki/data";
+
+    twikiName     = "Program Transformation Wiki";
+    scriptUrlPath = "/pt/bin";
+    pubUrlPath    = "/pt/pub";
+    dispScriptUrlPath = "/";
+    dispViewPath  = "";
+
+    startWeb = "Transform/WebHome";
+  };
+
+}
