0a9da158356885ff2578acb51f852bdeae23576e
svn <svn@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2005-04-29 13:47:39 +0000

    * Subversion server configuration.
    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@2942 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- subversion/svn-server-conf.nix
+++ subversion/svn-server-conf.nix
@@ -3,21 +3,21 @@
 import ./svn-server.nix (rec {
 
   # Location of all mutable data (except temporary files).
-  dataDir = "/home/eelco/services/data";
+  dataDir = "/data/subversion";
 
   # Temporary files.
-  tmpDir = "/home/eelco/tmp";
+  tmpDir = "/home/svn/tmp";
 
   # Subdirectory of `dataDir' to store logs.
   logsSuffix = if productionServer then "logs" else "testlogs";
 
   # Do we want SSL?  If so, specify the server certificate and key.
-  enableSSL = false;
+  enableSSL = true;
   sslServerCert = "/home/svn/ssl/server.crt";
   sslServerKey = "/home/svn/ssl/server.key";
 
   # Host name and ports to use.
-  hostName = "itchy.labs.cs.uu.nl";
+  hostName = "svn.cs.uu.nl";
   httpPort = if productionServer then 12080 else 12081;
   httpsPort = if productionServer then 12443 else 12444;
   defaultPort = if enableSSL then httpsPort else httpPort;
