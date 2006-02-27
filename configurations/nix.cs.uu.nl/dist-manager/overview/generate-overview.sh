dfb28cb05bbfbe4f52f4cb175c25eace5a57c654
eelco <eelco@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2006-02-27 15:50:09 +0000

    * Doh!
    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@4918 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- configurations/nix.cs.uu.nl/dist-manager/overview/generate-overview.sh
+++ configurations/nix.cs.uu.nl/dist-manager/overview/generate-overview.sh
@@ -13,11 +13,11 @@ cp failure.gif $out
 cp menuback.png $out
 chmod 644 $out/*.gif $out/*.png
 
-xsltproc -o $out/index.html main-index.xsl $src 
+xsltproc -o $out/index.xhtml main-index.xsl $src 
 xsltproc --stringparam out "$out" full-indices-per-package.xsl $src
 
-xsltproc -o $out/quick-view.html quick-view.xsl $src 
-xsltproc --stringparam sortByDate 1 -o $out/quick-view-by-date.html quick-view.xsl $src 
+xsltproc -o $out/quick-view.xhtml quick-view.xsl $src 
+xsltproc --stringparam sortByDate 1 -o $out/quick-view-by-date.xhtml quick-view.xsl $src 
 xsltproc --stringparam out "$out" full-status-per-package.xsl $src
 
 xsltproc --stringparam baseURL "$baseURL" --stringparam sortByDate 1 \
