141c941f4f102a4fa886518ea98b9248d4c39c0b
svn <svn@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2006-12-06 14:46:59 +0000

    * Use viewvc 1.0.3.
    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@7244 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- subversion/src/viewvc/default.nix
+++ subversion/src/viewvc/default.nix
@@ -1,12 +1,12 @@
 {stdenv, fetchurl, python, substituter, reposDir, adminAddr, subversion}:
 
 stdenv.mkDerivation {
-  name = "viewvc-1.0.1";
+  name = "viewvc-1.0.3";
   builder = ./builder.sh;
   
   src = fetchurl {
-    url = http://viewvc.tigris.org/files/documents/3330/33320/viewvc-1.0.1.tar.gz;
-    md5 = "2e14b2aeadd4e9ddd6b3876ffd184e61";
+    url = http://www.tigris.org/files/documents/3330/34803/viewvc-1.0.3.tar.gz;
+    md5 = "3d44ad485d38bf9f61d8111661260b4a";
   };
   conf = ./viewvc.conf.in;
 
