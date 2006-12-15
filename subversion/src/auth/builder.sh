2e4b94d0b33df45e3d72859c1accf5777e7c8782
eelco <eelco@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2006-12-15 21:20:59 +0000

    * Don't let apxs write in the Nix store.
    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@7350 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- subversion/src/auth/builder.sh
+++ subversion/src/auth/builder.sh
@@ -1,8 +1,11 @@
-. $stdenv/setup
+source $stdenv/setup
 
 ensureDir $out/modules
 
-$apacheHttpd/bin/apxs -c $noauth -o mod_authn_noauth.la
-$apacheHttpd/bin/apxs -c $dyn -o mod_authz_dyn.la
+cp $noauth mod_authn_noauth.c
+$apacheHttpd/bin/apxs -c mod_authn_noauth.c -o mod_authn_noauth.la
+
+cp $dyn mod_authz_dyn.c
+$apacheHttpd/bin/apxs -c mod_authz_dyn.c -o mod_authz_dyn.la
 
 cp .libs/*.so $out/modules/
