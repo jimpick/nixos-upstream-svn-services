7100cf01702578b18afc5a54796f2106d8812acd
eelco <eelco@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2007-05-02 13:30:47 +0000

    * Don't try to chown if we're not root.
    git-svn-id: https://svn.nixos.org/repos/nix/services/trunk@8642 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- subversion/startup-hook.sh
+++ subversion/startup-hook.sh
@@ -2,7 +2,9 @@ echo "creating missing files / tables..."
 
 needDir() {
     mkdir -p "$1"
-    chown @user@.@group@ "$1"
+    if test "$(id -u)" = 0; then
+	chown @user@.@group@ "$1"
+    fi
     chmod 750 "$1"
 }
 
@@ -24,4 +26,6 @@ echo | @db4@/bin/db_load -t hash -T "@dbDir@/svn-users"
 (echo root; echo "*") | @db4@/bin/db_load -n -t hash -T "@dbDir@/svn-users" 2> /dev/null || true
 (echo root; echo "Subversion Server Admin") | @db4@/bin/db_load -n -t hash -T "@dbDir@/svn-fullnames" 2> /dev/null || true
 
-chown @user@.@group@ @dbDir@/*
+if test "$(id -u)" = 0; then
+    chown @user@.@group@ @dbDir@/*
+fi
